ee002e5ab4536fb2151a24a0e39f61e6d0151574
nestor <niu8753@163.com>
2017-07-24 16:05:37 +0800

    Ready for deployment to project

---
+++ learning_logs/models.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.models import User
+
+
+import sys    
+reload(sys)    
+sys.setdefaultencoding('utf8')   
+
+# Create your models here.
+class Topic(models.Model):
+    """用户学习主"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.text
+
+class Entry(models.Model):
+    """学到的有关某个主题的具体知识"""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField(topic)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        if len(self.text) > 50:
+            return self.text[:50] + "..."
+        else:
+            return self.text
+
